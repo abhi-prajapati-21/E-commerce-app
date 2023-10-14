4659be2230581ebb86ed3d53072114eba821a2c4
Abhishek <abhishek.prajapati@blobstation.com>
2023-10-14 13:54:38 +0530

    added auth form

--- client/app/Auth/page.jsx
+++ client/app/Auth/page.jsx
@@ -1,39 +1,77 @@
 "use client";
-import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
 import { Form, Input } from "antd";
-import './auth.css'
+import "./auth.css";
 import React, { useState } from "react";
 
 const page = () => {
-  const [isLogin, setIsLogin] = useState();
+  const [isSignup, setIsSignup] = useState(false);
 
   const authSubmitHandler = () => {};
 
-  const iconStyle = { fontSize: "1.3rem", padding: "0px 9px 0 0" };
+  const iconStyle = {
+    color: "grey",
+    fontSize: "1.3rem",
+    padding: "0px 9px 0 0",
+  };
+
+//   const conditionalLine = () => {
+//     isSignup ? (
+// ;
+//     ): (
+
+//     );
+//   }
 
   return (
-    <div className="auth-container">
-      <div className="auth-form-wrapper">
-        <Form name="auth-form" onFinish={authSubmitHandler}>
+    <div className="auth-container flex">
+      <div className="auth-form-wrapper flex">
+        <Form
+          name="auth-form"
+          className="auth-form flex"
+          onFinish={authSubmitHandler}
+        >
+          {
+            isSignup && 
+            <Form.Item
+            name="name"
+            rules={[{ required: true, message: "name is required" }]}
+            className="inputs"
+          >
+            <Input
+              type="text"
+              placeholder="Enter Your Name"
+              prefix={<UserOutlined style={iconStyle} />}
+            />
+          </Form.Item>
+          }
           <Form.Item
-            name="Email"
+            name="email"
             rules={[{ required: true, message: "email is required" }]}
+            className="inputs"
           >
             <Input
               type="text"
               placeholder="Enter Email"
-              prefix={<UserOutlined style={iconStyle} />}
+              prefix={<MailOutlined style={iconStyle} />}
             />
           </Form.Item>
-          <Form.Item name="Password">
-            <Input
+          <Form.Item name="password" className="inputs" rules={[{ required: true, message: "password is required" }]}>
+            <Input.Password
               type="text"
               placeholder="Enter Your Password"
               prefix={<LockOutlined style={iconStyle} />}
             />
           </Form.Item>
-          <button onClick={authSubmitHandler}>Login</button>
+          <button className="auth-btn" onClick={authSubmitHandler}>
+            {isSignup ? 'Signup' : 'Login'}
+          </button>
         </Form>
+        <div className="sign-footer flex">
+          <span className="footer-txt">{ isSignup ?
+          'Already have an Account ?' : 'Dont have an Account ?'
+          } <span onClick={() => setIsSignup(!isSignup)} className="txt-signup">{isSignup ? 'Login' : 'Signup'}</span></span>
+        </div>
       </div>
     </div>
   );
